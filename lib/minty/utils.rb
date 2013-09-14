4daf02b8af0010b625e76e632d2debd80853be97
Matt Bridges <mbridges.91@gmail.com>
2013-09-14 09:54:07 -0500

    Still better names...

--- lib/minty/utils.rb
+++ lib/minty/utils.rb
@@ -3,9 +3,9 @@ module Minty
     extend self
 
     def dollars(value)
-      left, right = value.to_f.to_s.split(".")
+      dollars, right = value.to_f.to_s.split(".")
       cents = right.ljust(2, "0")
-      "$ " << [left, cents].join(".")
+      "$ #{dollars}.#{cents}"
     end
   end
 end
